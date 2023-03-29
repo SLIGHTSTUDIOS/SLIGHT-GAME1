ffa1d10a01f5bf8d372c39af1698b834b415ef0d
STARLIGHTSTUDIOS1 <125996614+STARLIGHTSTUDIOS1@users.noreply.github.com>
2023-03-29 21:09:38 +0400

    Killing mechanism
    Player now can kill enemies.if the player is in range for killing an enemy we can kil
    if the player comes in sight of the guard the player has 3 sec to kill him
    if time runs out game over .if player gets to close to the enmy he has 3 sec again to kill him.
    like if he touches the enemy from back

---
+++ PRJ69/Assets/Scripts/KillingMechanism.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingMechanism : MonoBehaviour
+{
+    bool playerInKillZone;
+    GameObject enemy;
+    /*countdown variables.time till player dies afer he is exposed to gaurds.he can avoid this 
+     by killing them before tim runs out*/
+    float timeCountdown = 0f;
+    float TimeLeft = 3f;
+    bool enemyAlerted;
+    void Start()
+    {
+        timeCountdown = TimeLeft;
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && playerInKillZone)
+        {
+            Debug.Log("killed");
+            enemyAlerted = false;
+            Destroy(enemy);
+        }
+        if(enemyAlerted == true)
+        {
+            timeCountdown -= 1 * Time.deltaTime;
+            print(timeCountdown);
+        }
+        if(timeCountdown <= 0 )
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("enemy alerted");
+            enemyAlerted = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyVeiwArea")
+        {
+            Debug.Log("enemy alerted");
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            playerInKillZone = true;
+            enemy = collision.gameObject;
+        }
+       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            playerInKillZone = false;
+        }
+    }
+
+}
